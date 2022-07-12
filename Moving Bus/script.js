786bc83390e7f27d4098e1dec5e8e8e9b1784b37
Constantin <dontu.31@mail.ru>
2022-07-12 14:35:16 +0300

    undo

--- Moving Bus/script.js
+++ Moving Bus/script.js
@@ -1,10 +1,10 @@
 const bus = document.getElementById("bus");
 let busMoveX = 0;
 let busMoveY = 0;
-let changesList = [];
-let lastChange = {};
+const changesList = [];
+let lastChange;
 
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', (e) => {
   if (e.keyCode == 39) {
     addLastChange();
     busMoveX += 50;
@@ -21,14 +21,16 @@ document.addEventListener('keydown', function (e) {
     addLastChange();
     busMoveY += 50;
     bus.style.top = busMoveY + "px";
-  } else if (e.keyCode == 8) {
+  } else if (e.keyCode == 8 && changesList.length) {
     lastChange = changesList.slice(-1);
-    console.log(lastChange);
-    console.log(typeof(lastChange));
+    changesList.splice(-1,1);
+    bus.style.left = lastChange[0].moveX + "px";
+    bus.style.top = lastChange[0].moveY + "px";
+    busMoveX = lastChange[0].moveX;
+    busMoveY = lastChange[0].moveY;
   }
 })
 
-function addLastChange () {
+const addLastChange = () => {
   changesList.push({moveX: busMoveX, moveY: busMoveY});
-  console.log(changesList);
 }
